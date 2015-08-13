d3d0be104d24b3ca389feffb0886a5647541ded8
Zain Abidin <zabidin@novetta.com>
2015-08-13 17:18:11 -0400

    First commit

---
+++ WcfServiceLibrary/BasicService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class BasicService : IBasicService
+    {
+        public string GetData(string value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+            
+        public static void Main()
+        {
+            BasicHttpBinding binding = new BasicHttpBinding();
+
+            binding.Name = "Basic_Binding";
+            binding.HostNameComparisonMode = HostNameComparisonMode.StrongWildcard;
+            binding.Security.Mode = BasicHttpSecurityMode.None;
+
+            Uri baseAddress = new Uri("http://localhost:8733/Design_Time_Addresses/WcfServiceLibrary/BasicService/");
+            Uri address = new Uri("http://localhost:8733/Design_Time_Addresses/WcfServiceLibrary/BasicService/testservice");
+
+            ServiceHost serviceHost = new ServiceHost(typeof(BasicService), baseAddress);
+
+            serviceHost.AddServiceEndpoint(typeof(IBasicService), binding, address);
+
+            // Open the ServiceHostBase to create listeners and start listening for messages.
+            serviceHost.Open();
+
+            // The service can now be accessed.
+            Console.WriteLine("The service is ready.");
+            Console.WriteLine("Press <ENTER> to terminate service.");
+            Console.WriteLine();
+            Console.ReadLine();
+
+            // Close the ServiceHostBase to shutdown the service.
+            serviceHost.Close();
+        }
+    }
+}
